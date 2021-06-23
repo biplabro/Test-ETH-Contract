18d0b430b6d5ef2364f58ab2de157f7c25902e54
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-06-23 17:19:04 +0530

    commit

--- compile.js
+++ compile.js
@@ -1,1 +1,9 @@
-/*require ('./contracts/Inbox.sol') //bad*/
+/*require ('./contracts/Inbox.sol') //bad to execute the file directly, different than javaScript*/
+const path = require('path'); // correct approach is to read the file contents first & then feed it to compiler
+const fs = require('fs'); //path & fs are standard node modules
+const solc =require('solc') //link 'solc' compiler to solc variable
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol') //mention the path variable, __dirname = working directory, for cross platform compatibility
+const source = fs.readFileSync(inboxPath, 'utf8') //read the contents of the contract/file, mention encoding
+
+console.log(solc.compile(source,1)) //compile 'source' using 'solc' compiler, number of contracts = 1
