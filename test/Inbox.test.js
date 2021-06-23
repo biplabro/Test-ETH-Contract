7e694fc0760046188c0afac2930b9a91a2e8f3be
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-06-23 22:45:29 +0530

    added test script

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,3 +6,27 @@ const assert = require('assert')  // assurtion about tests; comparison. required
 const ganache = require('ganache-cli')  //to create local ETH test network
 const Web3 = require('web3')  // constructor function, uppercase
 const web3 = new Web3(ganache.provider());  // create Web3 instance
+
+
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+
+describe('Car Condition',() => {
+
+  it('can park', () => {
+    const car = new Car();
+    assert.equal(car.park(), 'stopped')
+  });
+
+  it('can drive', () => {
+    const car = new Car();
+    assert.equal(car.drive(), 'vroom')
+  });
+});
