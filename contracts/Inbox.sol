9fd8bc49d528f664f864e78772730da77f06545d
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-06-23 17:00:53 +0530

    commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;        //specifies solidity compiler version, version identifier
+
+contract Inbox {                //contract definition, similar to "class" concept
+    string public message;      //message = storage variable, contains string, Public = access rights
+
+    function Inbox(string initialMessage) public {      // Inbox = constructor
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    /*function getMessage() public view returns (string) {    //getMessage with no arguments, type = public view, return type string
+        return message; //redundant function
+    }*/
+}
