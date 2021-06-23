6df59cfe98d7ec567cc83f9cefc5c307dbb23619
Biplab Roy <35135009+biplabro@users.noreply.github.com>
2021-06-23 18:29:16 +0530

    commit

--- compile.js
+++ compile.js
@@ -6,5 +6,16 @@ const solc =require('solc') //link 'solc' compiler to solc variable
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol') //mention the path variable, __dirname = working directory, for cross platform compatibility
 const source = fs.readFileSync(inboxPath, 'utf8') //read the contents of the contract/file, mention encoding
 
+// There are 2 snippets, either 1 can be used
+/*
+// snippet 1
 console.log(solc.compile(source,1)) //compile 'source' using 'solc' compiler, number of contracts = 1
 // wrapping compiler call inside console.log to see whats happening inside the code
+// executing the code in terminal node compile.js ( at working directory)
+// it returns the contracts object, inside which another object called ':Inbox' is created, it contains two main properties, 
+// bytecode = to ne stored & executed on the blockchain,
+// interface = the ABI of the contract
+*/
+
+// snippet 2
+module.exports = solc.compile(source,1).contracts[':Inbox'] //compile & export the contract properties making it an available resource for the rest of the codebase
